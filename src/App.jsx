acc53051c7473925548b4e03fbacd27f5268c551
Ifaz Ahmed <gdneop@gmail.com>
2022-10-28 16:34:43 +0100

    Create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,50 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
+import "./css/App.css";
+
+import {
+  HomeModernIcon,
+  Cog6ToothIcon,
+} from "@heroicons/react/24/solid";
+
+import {  
+  HeartIcon,
+} from "@heroicons/react/24/outline";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="app">
+      <div className="nav">
+        <div className="navButton">
+          <HomeModernIcon className="navIcon" />
+          <p className="navText">Home</p>
+        </div>
 
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
+        <div className="navButton">
+          <svg className="navIcon" width="34" height="34" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="19.8333" cy="24.0833" r="4.25" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M24.0833 24.0833V5.66667H29.75" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M18.4167 7.08333H4.25" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M4.25 12.75H18.4167" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+            <path d="M12.75 18.4167H4.25" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+          </svg>
+          <p className="navText">Playlists</p>
+        </div>
 
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+        <div className="navButton right">
+          <Cog6ToothIcon className="navIcon" />
+          <p className="navText">Settings</p>
+        </div>
 
-      <div className="row">
-        <div>
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="button" onClick={() => greet()}>
-            Greet
-          </button>
+        <div className="title">
+          <HeartIcon className="titleIcon" />
+          <p className="titleText">Phoniq</p>
+          <HeartIcon className="titleIcon" />
         </div>
       </div>
-
-      <p>{greetMsg}</p>
     </div>
   );
 }
